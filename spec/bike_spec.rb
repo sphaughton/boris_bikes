eb1fa6631f408d316019def98b26f6c6a10bdead
Sean Haughton <seanphaughton@gmail.com>
2015-02-03 13:55:19 +0000

    Tests passing

--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -1,6 +1,8 @@
+require 'bike'
+
 describe Bike do 
   it 'should not be broken after we create it' do
     the_bike = Bike.new
-    expect(the_bike).not_to_be_broken
+    expect(the_bike).not_to be_broken
   end  
 end
